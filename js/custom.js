a28d65b19e0d49f349674adb2184388f02e9b866
unknown <snow28@ukr.net>
2018-01-31 10:45:44 +0100

    slider-not-ready

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-    $('.your-class').slick({
+    $('.slider__wrapper-js').slick({
+    });
 
-});
+    $('.header__popup-js').on('click',function(){
+        $('.popup-window').toggleClass('popup-window-active');
+    });
+    $('#button-popup-close').on('click',function(){
+        $('.popup-window').toggleClass('popup-window-active');
+    });
 });
